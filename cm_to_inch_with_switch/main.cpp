6947335b1995801409357251b0fe22fb2f5f797d
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-21 11:47:34 +0200

    added cm to inch with switch convertor

---
+++ cm_to_inch_with_switch/main.cpp
@@ -0,0 +1,26 @@
+#include "../std_lib_facilities.h"
+
+// converts from inches to centimeters or centimeters to inches
+// a suffix 'i' or 'c' indicates the unit of the input
+// any other suffic is an error
+int main()
+{
+    constexpr double cm_per_inch = 2.54;
+    double length = 1;
+    char unit = 0;
+
+    cout << "Please enter a length followed by a unit (c or i):\n";
+    cin >> length >> unit;
+
+    switch (unit) {
+    case 'i':
+        cout << length << "in == " << cm_per_inch*length << "cm\n";
+        break;
+    case 'c':
+        cout << length << "cm == " << length/cm_per_inch << "in\n";
+        break;
+    default:
+        cout << "Sorry, I don't know a unit called '" << unit << "'\n";
+        break;
+    }    
+}
