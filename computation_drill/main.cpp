d120ecf7bba10e4db9c12f5957959ad0145144ea
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-22 00:58:19 +0200

    computation drill project set up

---
+++ computation_drill/main.cpp
@@ -0,0 +1,10 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    cout << "Please enter pair of numbers: ";
+    cin >> num1 >> num2;
+    cout << "The numbers are: " << num1 << " " << num2;
+}
