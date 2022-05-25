b421affbc6b399ed38ceb5a275bff6bd52b97f40
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-25 20:30:45 +0200

    initialized pointers arithmetic project

---
+++ pointers_arithmetic/main.cpp
@@ -0,0 +1,12 @@
+#include "../std_lib_facilities.h"
+int main()
+{
+    const int SIZE = 5;
+    int arr[SIZE]{4,55,79,1,4};
+
+    for(int i = 0; i < SIZE; i++){
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
