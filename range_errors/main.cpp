af5c05e278e9b377582e8b5b2dfc88b624cf6ae3
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-26 11:15:39 +0200

    added range error

---
+++ range_errors/main.cpp
@@ -0,0 +1,13 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    vector<int> v;
+    for(int i; cin>>i; )
+        v.push_back(i);
+
+    for(int i = 0; i<=v.size(); ++i)
+        cout << "v[" << i << "] == " << v[i] << '\n';
+    
+    return 0;
+}
