cef21a399fa806807f308ce0408949fe1fa93b71
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-26 12:28:40 +0200

    added try catch to open file using ifstream

---
+++ bad_input_errors/try_catch.cpp
@@ -0,0 +1,22 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    string path = "myFile.txt";
+
+    ifstream fin;
+
+    try
+    {
+        cout << "Trying to open file!" << endl;
+        fin.open(path);
+        cout << "File successfully opened" << endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+    return 0;
+}
