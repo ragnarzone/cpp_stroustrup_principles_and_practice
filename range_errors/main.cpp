05d29e9ce114a5cc9390a5ef3aa4b164fb35607e
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-26 11:22:31 +0200

    added try-catch block in range errors

--- range_errors/main.cpp
+++ range_errors/main.cpp
@@ -2,12 +2,20 @@
 
 int main()
 {
-    vector<int> v;
-    for(int i; cin>>i; )
-        v.push_back(i);
+    try {
+        vector<int> v;
+        for(int i; cin>>i; )
+            v.push_back(i);
 
-    for(int i = 0; i<=v.size(); ++i)
-        cout << "v[" << i << "] == " << v[i] << '\n';
-    
+        for(int i = 0; i<=v.size(); ++i)
+            cout << "v[" << i << "] == " << v[i] << '\n';
+    } catch(out_of_range) {
+        cerr << "Oops! Range error\n";
+        return 1;
+    } catch(...) {
+        cerr << "Exception: something went wrong\n";
+        return 2;
+    }
+
     return 0;
 }
