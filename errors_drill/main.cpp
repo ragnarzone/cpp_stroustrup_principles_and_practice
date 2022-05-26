6d7474c93a19aa3cb8d7f7962fb943218ca8663f
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-27 00:44:51 +0200

    finished errors drill

--- errors_drill/main.cpp
+++ errors_drill/main.cpp
@@ -14,8 +14,20 @@ int main()
         // bool c = false; if (!c) cout << "Success!\n"; else cout << "Fail!\n";
         // string s = "ape"; bool c = "fool">s; if (c) cout << "Success!\n";
         // string s = "ape"; if (s!="fool") cout << "Success!\n";
-        string s = "ape"; if (s!="fool") cout << "Success!\n";
-
+        // string s = "ape"; if (s!="fool") cout << "Success!\n";
+        // string s = "ape"; if (s+"fool"=="apefool") cout << "Success!\n";
+        // vector<char> v(5); for (int i=0; i<v.size(); ++i) ; cout << "Success!\n";
+        // vector<char> v(5); for (int i=0; i<v.size(); ++i) ; cout << "Success!\n";
+        // string s = "Success!\n"; for (int i=0; i<9; ++i) cout << s[i];
+        // if (true) cout << "Success!\n"; else cout << "Fail!\n";
+        // int x = 2000; int c = x; if (c==2000) cout << "Success!\n"; 
+        // string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
+        // int i=0; int j = 9; while (i<=10) ++i; if (j<i) cout << "Success!\n";
+        // int x = 2; double d = 5/(x-1); if (d==2*x+1) cout << "Success!\n";
+        // string s = "Success!\n"; for (int i=0; i<=10; ++i) cout << s[i];
+        // int i=0; int j = 8; while (i<10) ++i; if (j<i) cout << "Success!\n";
+        // int x = 4; double d = 5/(x-2); if (d=0.5*x+0.5) cout << "Success!\n";
+        cout << "Success!\n";
 
 
         keep_window_open();
