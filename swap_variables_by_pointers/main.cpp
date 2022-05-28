9c0686ceaa6d0092a01cf0dfd2db9b587e4b8dcf
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-28 11:52:21 +0200

    added swap variables by pointers

---
+++ swap_variables_by_pointers/main.cpp
@@ -0,0 +1,24 @@
+#include "../std_lib_facilities.h"
+
+void Swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int var1 = 31;
+    int var2 = 64;
+
+    cout << "var1\t" << var1 << endl;
+    cout << "var2\t" << var2 << endl;
+
+    cout << "Swap" << endl;
+
+    Swap(&var1, &var2);
+
+    cout << "var1\t" << var1 << endl;
+    cout << "var2\t" << var2 << endl;
+}
