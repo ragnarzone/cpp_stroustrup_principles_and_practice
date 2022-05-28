b160c3e327c810dff410346f52ad36064c29c9ad
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-28 14:17:08 +0200

    added NULL and nullptr

--- dynamic_memory_allocation/main.cpp
+++ dynamic_memory_allocation/main.cpp
@@ -6,4 +6,13 @@ int main()
     *pa = 10;
     cout << *pa << endl;
     delete pa;          // delete this 4 bytes
+
+    pa = NULL;          // reffer our pointer to 0 in memory
+    // pa = nullptr;    // nullptr added in C++ 11std
+                        // first delete data and then clean pointer with nullptr
+
+    if (pa != NULL){
+        cout << pa << endl;
+    }
+    delete pa;
 }
